c72b81164a1237521ec304674205d1ee9247a6bd
bowerman4875 <bowerman.andrew@gmail.com>
2011-09-24 17:02:45 -0700

    Initial import.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Hsm::Application.config.secret_token = '83983daf3f74078cb7bd785cd467d77b00013beee8e336bc4b86d19a45d44d4ea2a36e5860f379ec0f07f60eb4b51262f15d642a6a4526402bb00f42834c7634'
