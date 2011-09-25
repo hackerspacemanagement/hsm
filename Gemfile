3a3b1dc7bc193d4c6adc1f7869f49eed9de2bfab
bowerman4875 <bowerman.andrew@gmail.com>
2011-09-24 23:53:46 -0700

    Added twitter bootstrap, adjusted devise to work with bootstrap

--- Gemfile
+++ Gemfile
@@ -6,6 +6,7 @@ gem 'devise'
 gem 'haml', '>= 3.0.0'
 gem 'haml-rails'
 gem 'jquery-rails'
+gem 'dynamic_form'
 
 # Gems used only for assets and not required
 # in production environments by default.
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Hsm::Application.routes.draw do
   devise_for :users
-
+  resource :home
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
@@ -50,7 +50,7 @@ Hsm::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  # root :to => 'welcome#index'
+  root :to => 'home#index'
 
   # See how all your routes lay out with "rake routes"
 
