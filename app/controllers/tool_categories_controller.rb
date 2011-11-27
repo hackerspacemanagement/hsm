a037e04d66ce9035024b9a2b0d7d4935a9b8adfe
Ryan Rix <ry@n.rix.si>
2011-11-26 20:07:43 -0700

    Add tool_category controller
    This lets admins add and remove Categories which the tools can be associated
    with.

---
+++ app/controllers/tool_categories_controller.rb
@@ -0,0 +1,36 @@
+class ToolCategoriesController < ApplicationController
+  def index
+    @categories = ToolCategory.all
+  end
+  
+  def new
+    @category = ToolCategory.new
+  end
+  
+  def edit
+    @category = ToolCategory.find(params[:id])
+  end
+  
+  def create
+    @category = ToolCategory.new(params[:tool_category])
+    if @category.save
+      flash[:notice] = "Dude, you are awesome. You've added a category."
+      redirect_to tool_category_path
+    else
+      flash[:alert] = 'You broke something. :-('
+      redirect_to edit_tool_category_path(@category)
+    end
+  end
+
+  def update
+    @category = ToolCategory.find(params[:id])
+    if @tool.update_attributes(params[:tool_category])
+      flash[:notice] = "Your changes have been saved, good sir or madam."
+      redirect_to tool_category_path
+    else
+      flash[:alert] = "Whoops, something bad happened!"
+      redirect_to edit_tool_category_path(@category)
+    end
+  end
+
+end
---
+++ app/helpers/tool_categories_helper.rb
@@ -0,0 +1,2 @@
+module ToolCategoriesHelper
+end
