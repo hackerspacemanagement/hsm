7217c3a0bcaa4774c078cff39f3f51100f1804ca
Trever Fischer <tdfischer@fedoraproject.org>
2011-11-27 23:42:43 -0500

    Make the mail address configurable

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -1,10 +1,12 @@
+Settings.defaults[:mail_from_address] = 'hsm@localhost'
+
 # Use this hook to configure devise mailer, warden hooks and so forth. The first
 # four configuration values can also be set straight in your models.
 Devise.setup do |config|
   # ==> Mailer Configuration
   # Configure the e-mail address which will be shown in Devise::Mailer,
   # note that it will be overwritten if you use your own mailer class with default "from" parameter.
-  config.mailer_sender = 'info@heatsynclabs.org'
+  config.mailer_sender = Settings.mail_from_address
 
   # Configure the class responsible to send e-mails.
   # config.mailer = "Devise::Mailer"
--- config/initializers/settings.rb
+++ config/initializers/settings.rb
@@ -1,1 +1,3 @@
+# See devise.rb for where we set the default mailing address
+
 Settings.defaults[:group_name] = 'Your Friendly Hackerspace'
