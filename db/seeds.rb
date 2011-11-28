ac558d7ae1365974b0cf0c6048dd24f735139a98
Trever Fischer <tdfischer@fedoraproject.org>
2011-11-28 01:22:44 -0500

    Create admin user as part of admin group and invoke new db:permissions task as part of db:seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,15 +6,26 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
+puts 'Setting up administrator role.'
+role = Role.new :name => 'Administrator',
+                :description => 'Administrators have absolute power and inherently have all roles'
+role.save rescue ActionView::Template::Error
+role.save
+
+puts "Role: #{ role.name } created!"
+
 puts 'Setting up default user.'
 user = User.new :first_name            => 'Test',
                 :last_name             => 'User',
                 :email                 => 'user@example.com',
                 :password              => 'password',
-                :password_confirmation => 'password'
+                :password_confirmation => 'password',
+                :role                  => role
 
 user.save rescue ActionView::Template::Error
 user.save
 user.confirm!
 
 puts "User: #{ user.full_name } - #{ user.email } created and confirmed!"
+
+Rake::Task['db:permissions'].invoke
