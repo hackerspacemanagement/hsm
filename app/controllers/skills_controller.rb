03f85720f2cc8ababbf528d8579ca27dc7764325
Ryan Rix <ry@n.rix.si>
2011-12-23 00:59:03 -0700

    Make the skills VC work

--- app/controllers/skills_controller.rb
+++ app/controllers/skills_controller.rb
@@ -1,13 +1,19 @@
 class SkillsController < ApplicationController
     def index
-        if params[:id] then
-            @user = User.find(params[:id])
-            @skills = @user.skills
-        else
-            @skills = Skill.all
-        end
     end
     
+    def create
+        @skill = Skill.new(params[:skill])
+        if @skill.save
+            flash[:notice] = "Dude, you are awesome. You've added a skill."
+            redirect_to users_skill_path
+        else
+            flash[:alert] = 'You broke something. :-('
+            redirect_to users_skill_path
+        end
+    end
+  
     def new
+        @skill = Skill.new
     end
 end
