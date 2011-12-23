efa2da12190269b492e6a74960e383cb69ee4550
Ryan Rix <ry@n.rix.si>
2011-12-23 01:16:28 -0700

    Add users_skills#create method

--- app/controllers/users_skills_controller.rb
+++ app/controllers/users_skills_controller.rb
@@ -11,5 +11,17 @@ class UsersSkillsController < ApplicationController
         @users_skills = UsersSkill.new
         @all_skills = Skill.all.collect {|s| [ s.name, s.id ] }
         @proficiency_levels = [0,1,2,3,4,5]
+    end   
+    
+    def create
+        @users_skill = UsersSkill.new(params[:users_skill])
+        @users_skill.user_id = current_user.id
+        if @users_skill.save
+            flash[:notice] = "Dude, you are awesome. You've added a tool."
+            redirect_to users_skills_path
+        else
+            flash[:alert] = 'You broke something. :-('
+            redirect_to edit_users_skills_path(@users_skill)
+        end
     end
 end
