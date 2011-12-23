99e94575218c3afb1da3f3c0aaf9d6a36ce38db3
Ryan Rix <ry@n.rix.si>
2011-12-23 01:46:20 -0700

    Change the redirects to not be stupid

--- app/controllers/skills_controller.rb
+++ app/controllers/skills_controller.rb
@@ -6,10 +6,10 @@ class SkillsController < ApplicationController
         @skill = Skill.new(params[:skill])
         if @skill.save
             flash[:notice] = "Dude, you are awesome. You've added a skill."
-            redirect_to users_skill_path
+            redirect_to edit_users_skill_path
         else
             flash[:alert] = 'You broke something. :-('
-            redirect_to users_skill_path
+            redirect_to edit_skills_path
         end
     end
   
