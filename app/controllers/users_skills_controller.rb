9279bb3b310fc3501e8216b694bc43ae2dae4d47
Ryan Rix <ry@n.rix.si>
2011-12-23 00:43:41 -0700

    Make the users_skills controller work properly

--- app/controllers/users_skills_controller.rb
+++ app/controllers/users_skills_controller.rb
@@ -1,2 +1,15 @@
 class UsersSkillsController < ApplicationController
+    def index
+       if params[:id] then
+           @uskills = UsersSkill.find(params[:id])
+       else
+           @uskills = UsersSkill.all
+       end
+    end
+    
+    def new
+        @users_skills = UsersSkill.new
+        @all_skills = Skill.all.collect {|s| [ s.name, s.id ] }
+        @proficiency_levels = [0,1,2,3,4,5]
+    end
 end
