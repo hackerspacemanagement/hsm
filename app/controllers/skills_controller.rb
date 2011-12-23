1a2fded953d6a1b6d5f478114405a136592b1337
Ryan Rix <ry@n.rix.si>
2011-12-22 23:13:46 -0700

    Create index of skills view
    Pretty basic stuff, like always

--- app/controllers/skills_controller.rb
+++ app/controllers/skills_controller.rb
@@ -1,2 +1,13 @@
 class SkillsController < ApplicationController
+    def index
+        if params[:id] then
+            @user = User.find(params[:id])
+            @skills = @user.skills
+        else
+            @skills = Skill.all
+        end
+    end
+    
+    def new
+    end
 end
--- app/models/skill.rb
+++ app/models/skill.rb
@@ -1,4 +1,4 @@
 class Skill < ActiveRecord::Base
-    has_many :users_skills, :dependent => true
+    has_many :users_skills
     has_many :users, :through => :users_skills
 end
