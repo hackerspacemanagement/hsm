34652e234b06358e617ce0d438c9d63d705c4321
Ryan Rix <ry@n.rix.si>
2011-12-26 13:35:58 -0700

    Beginning Required Role skills
    So this is the first commit in the line to solve the problem of "how do we
    actually know this user can do that".
    
    At heatsync, we have a lot of tools that simply shouldn't be used unless you
    are well versed in them (google "lathe accidents", even though it's NSFL).
    These will be controlled soon by RFID interlocks with a central DB to manage
    them. HSM should be that DB, but righ tnow anyone can just say "I AM TEH
    AWESOME AT LATHE-- *dead*" and that would suck.
    
    I'm going to be setting this up so that only certain users can grant certain
    skills to other users. This will replace the @user.can_fly magick that
    Trever added when we were still trying to figure out how permissions would
    work, at least for this usecase. .can_fly magick can still be used for
    administravia on the site itself, I think, since "adding users to HSM" skill
    isn't really optimal.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20111223042943) do
+ActiveRecord::Schema.define(:version => 20111226202228) do
 
   create_table "permissions", :force => true do |t|
     t.string   "name"
@@ -52,6 +52,7 @@ ActiveRecord::Schema.define(:version => 20111223042943) do
 >>>>>>> Set up relationships
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "role_required_to_grant"
   end
 
   create_table "tool_categories", :force => true do |t|
