9f7ecf72fa076e072e28f7f826b0dd589bf1819a
Ryan Rix <ry@n.rix.si>
2011-12-27 09:14:04 -0700

    Add validations

--- app/models/users_skill.rb
+++ app/models/users_skill.rb
@@ -1,4 +1,8 @@
 class UsersSkill < ActiveRecord::Base
     belongs_to :skill
     belongs_to :user
+  
+    validates_presence_of :skill_id
+    validates_presence_of :user_id  
+    validates_presence_of :proficiency
 end
