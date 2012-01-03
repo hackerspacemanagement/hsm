ec27473ec6a2287a02e7e271c637951fb6eed977
Ryan Rix <ry@n.rix.si>
2012-01-02 21:53:09 -0700

    Add settings config pages

--- app/controllers/settings_controller.rb
+++ app/controllers/settings_controller.rb
@@ -1,2 +1,10 @@
 class SettingsController < ApplicationController
+    def index
+        @config = Settings.all
+    end
+    
+    def create
+        Settings.group_name = params[:value][:group_name]
+        redirect_to settings_path
+    end
 end
