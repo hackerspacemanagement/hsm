8b545af504d5044e5339db8a9a6b3ab61c00040d
Ryan Rix <ry@n.rix.si>
2012-01-06 22:57:10 -0700

    Some standard seeds taken from Toolshare-Devise

--- db/seeds.rb
+++ db/seeds.rb
@@ -25,15 +25,91 @@ role = Role.new :name => 'User',
 role.save
 puts "Role: #{ role.name } created!"
 
-puts 'Setting up default usersdb/seeds.rb.'
-user = User.new :first_name            => 'Test',
-                :last_name             => 'User',
-                :email                 => 'user@example.com',
-                :password              => 'password',
-                :password_confirmation => 'password',
-                :role                  => adminRole
+tool = ToolCategory.new( :name => 'Woodworking Tools' )
+tool.save rescue ActionView::Template::Error
 
+tool = ToolCategory.new( :name => 'Measuring Tools' )
+tool.save rescue ActionView::Template::Error
+
+tool = ToolCategory.new( :name => 'Metalworking Tools' )
+tool.save rescue ActionView::Template::Error
+
+tool = ToolCategory.new( :name => 'Power Tool (other)' )
+tool.save rescue ActionView::Template::Error
+
+tool = ToolCategory.new( :name => 'Electronics Tools' )
+tool.save rescue ActionView::Template::Error
+
+tool = ToolCategory.new( :name => 'Hand Tool (other)' )
+tool.save rescue ActionView::Template::Error
+
+tool = ToolCategory.new( :name => 'Containers and Transportation' )
+tool.save rescue ActionView::Template::Error
+
+tool = ToolCategory.new( :name => 'Automotive Tools' )
+tool.save rescue ActionView::Template::Error
+
+tool = ToolCategory.new( :name => 'Pneumatic Tools (other)' )
+tool.save rescue ActionView::Template::Error
+
+tool = ToolCategory.new( :name => 'Flammable Gas Tools (other)' )
+tool.save rescue ActionView::Template::Error
+
+tool = ToolCategory.new( :name => 'Fastening & Joining Tools' )
+tool.save rescue ActionView::Template::Error
+
+tool = ToolCategory.new( :name => 'Rapid Prototyping Tools & Equipment' )
+tool.save rescue ActionView::Template::Error
+
+tool = ToolCategory.new( :name => 'Computer & Networking Tools' )
+tool.save rescue ActionView::Template::Error
+
+tool = ToolCategory.new( :name => 'Electronics (other)' )
+tool.save rescue ActionView::Template::Error
+
+tool = ToolCategory.new( :name => 'Electronics Supplies' )
+tool.save rescue ActionView::Template::Error
+
+tool = ToolCategory.new( :name => 'Fasteners & Supplies' )
+tool.save rescue ActionView::Template::Error
+
+tool = ToolCategory.new( :name => 'Fabric & Textiles' )
+tool.save rescue ActionView::Template::Error
+
+tool = ToolCategory.new( :name => 'Other' )
+tool.save rescue ActionView::Template::Error
+
+tool = ToolCategory.new( :name => 'Books & Resources' )
+tool.save rescue ActionView::Template::Error
+
+tool = ToolCategory.new( :name => 'Computer & Networking Devices' )
+tool.save rescue ActionView::Template::Error
+
+tool = ToolCategory.new( :name => 'Lighting' )
+tool.save rescue ActionView::Template::Error
+
+tool = ToolCategory.new( :name => 'Locks & Picks' )
+tool.save rescue ActionView::Template::Error
+
+tool = ToolCategory.new( :name => 'Clamps' )
+tool.save rescue ActionView::Template::Error
+
+tool = ToolCategory.new( :name => 'Safety' )
+tool.save rescue ActionView::Template::Error
+
+tool = ToolCategory.new( :name => 'Cutting Tools' )
+tool.save rescue ActionView::Template::Error
+
+user = User.new( :email => 'ry@n.rix.si',
+                 :first_name => '2011-03-28',
+                 :last_name => '05:40:12.130017+0000')
 user.save
 user.confirm!
+user.confirmation_token = 'KOASEkkoAdwmgTz1KZzW'
+user.encrypted_password = 'ea79be64094683ab971c13eccf229cd3c497c5b9'
+user.save
 
-puts "User: #{ user.full_name } - #{ user.email } created and confirmed!"
+puts "Setting up default Settings"
+Settings.group_name = "Hackerspace"
+
+Rake::Task['db:permissions'].invoke
