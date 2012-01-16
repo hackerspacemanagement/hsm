f221afbd82b20e396de2b64c474355f8ff628bad
Ryan Rix <ry@n.rix.si>
2012-01-16 11:16:10 -0700

    Only show the admin page if user can manage all
    I like how that verbage just works.. It's not DRY yet though.

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -1,5 +1,14 @@
 class Admin::UsersController < ApplicationController
-  before_filter :authenticate_user!
+   before_filter :authenticate
+  
+  # Where can I move this for DRY -rr
+  def authenticate
+    if current_user and current_user.can? :manage, :roles
+      return
+    end
+    flash[:alert] = "You aren't authorized to view this!"
+    redirect_to root_path
+  end
   
   def index
     @users = User.all
