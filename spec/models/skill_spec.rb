23e6b7e965868c40dffef25459fa5691c77406df
Ryan Rix <ry@n.rix.si>
2012-01-25 03:13:35 -0700

    Skill spec
    just basic verification, the meat is in users_skill_spec after all.

--- spec/models/skill_spec.rb
+++ spec/models/skill_spec.rb
@@ -1,5 +1,23 @@
 require 'spec_helper'
 
 describe Skill do
-  pending "add some examples to (or delete) #{__FILE__}"
+    describe "creating skills" do
+        before do
+            @skill = Factory.create :skill, :name => "Test Skill"
+        end
+        
+        it "can create skills" do
+            @skill.name.should == "Test Skill"
+        end
+        
+        it "can change name" do
+            @skill.name = "Important Thing"
+            @skill.name.should == "Important Thing"
+        end
+        
+        it "can change description" do
+            @skill.description = "This is important!"
+            @skill.description.should == "This is important!"
+        end
+    end
 end
