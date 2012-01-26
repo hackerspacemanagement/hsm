aadfa54b1687341ecba750a1cd552ffe4c9f9dd0
Jose Diaz <jose.a.diaz@aguaseca.com>
2012-01-25 17:09:42 -0700

    removed Faker

--- spec/factories.rb
+++ spec/factories.rb
@@ -1,9 +1,9 @@
 FactoryGirl.define do
 
-  factory :user do
-    first_name              Faker::Name.first_name
-    last_name               Faker::Name.last_name
-    email                   Faker::Internet.email
+  factory :user, :class => User do
+    first_name              'first_name'
+    last_name               'last_name'
+    email                   'first_name.last_name@example.com'
     password                'password'
     password_confirmation   'password'
   end
