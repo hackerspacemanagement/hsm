b1587db1331093635479f37a87f410ad4014fdc0
Ryan Rix <ry@n.rix.si>
2012-01-28 01:42:57 -0700

    Move skill addition tests to skill_spec
    This is the other half of creating assign_to in the Skill model so that
    things are nice and there are no ugly hacks around the tests.

--- spec/models/skill_spec.rb
+++ spec/models/skill_spec.rb
@@ -20,4 +20,44 @@ describe Skill do
             @skill.description.should == "This is important!"
         end
     end
+     
+    describe "restricted skill addition" do
+        before do
+            @permission = Permission.create :name => 'administrate'
+            @permission.save 
+            
+            @adminrole = Factory.create :role, :name => "Administrator",
+                                               :permissions => [Permission.where(:name => 'administrate').first]
+            @admin = Factory.create :user, :first_name => "Admin",
+                                           :last_name  => "User",
+                                           :email      => "admin@example.com",
+                                           :role => @adminrole
+            
+            @regularuser = Factory.create :user, :first_name => "Regular",
+                                                 :last_name  => "User",
+                                                 :email      => "me@example.com"
+            @regularuser.confirm!
+            
+            @restrictedSkill = Factory.create :skill, :name => "Restricted Skill",
+                                                      :role_required_to_grant => @adminrole
+        end
+        
+        it "should allow admin to add any skill" do
+            @restrictedSkill.assign_to @regularuser, @admin, 1
+            
+            @regularuser.skills.should have(1).skill
+        end
+        
+        it "should not allow regular users to add restricted skills to themselves" do
+            @restrictedSkill.assign_to @regularuser, @regularuser, 1
+                                                   
+            @regularuser.skills.should have(0).skill
+        end
+        
+        it "should not allow regular users to add restricted skills to anyone else" do
+            @restrictedSkill.assign_to @regularuser, @admin, 1
+            
+            @admin.skills.should have(0).skill
+        end
+    end
 end
