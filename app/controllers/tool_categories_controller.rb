f257ff3678c67876de174b4921c6cc9b86eb958b
Ryan Rix <ry@n.rix.si>
2012-01-30 14:44:40 -0700

    Fix paths

--- app/controllers/tool_categories_controller.rb
+++ app/controllers/tool_categories_controller.rb
@@ -1,7 +1,6 @@
 class ToolCategoriesController < ApplicationController
   before_filter :authenticate_user!, :except => [:index, :show]
 
-
   def index
     @categories = ToolCategory.all
   end
@@ -18,7 +17,7 @@ class ToolCategoriesController < ApplicationController
     @category = ToolCategory.new(params[:tool_category])
     if @category.save
       flash[:notice] = "Dude, you are awesome. You've added a category."
-      redirect_to tool_category_path
+      redirect_to new_tool_path
     else
       flash[:alert] = 'You broke something. :-('
       redirect_to edit_tool_category_path(@category)
@@ -29,7 +28,7 @@ class ToolCategoriesController < ApplicationController
     @category = ToolCategory.find(params[:id])
     if @tool.update_attributes(params[:tool_category])
       flash[:notice] = "Your changes have been saved, good sir or madam."
-      redirect_to tool_category_path
+      redirect_to tool_categories_path
     else
       flash[:alert] = "Whoops, something bad happened!"
       redirect_to edit_tool_category_path(@category)
