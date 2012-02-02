7178c250bda958b281797e838dfd1690ea45a5c6
Jose Diaz <jose.a.diaz@aguaseca.com>
2012-02-01 18:33:13 -0700

    properly seed mongo database

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,34 +6,10 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
-puts 'Setting up permissions.'
-Rake::Task['db:permissions'].invoke
+puts 'Emptying the MongoDB Database'
+Mongoid.master.collections.select { |c| c.name !~ /system/ }.each(&:drop)
 
-puts 'Setting up administrator role.'
-adminRole = Role.new :name => 'Administrator',
-                :description => 'Administrators have absolute power and inherently have all roles',
-                :permissions => [Permission.where(:name => 'administrate').first]
-                
-adminRole.save
-puts "Role: #{ adminRole.name } created!"
+puts 'Setting up default user account'
+user = User.create! :email => 'user@example.com', :password => 'password', :password_confirmation => 'password'
 
-puts 'Setting up user role.'
-role = Role.new :name => 'User',
-                :description => 'Users may login',
-                :permissions => [Permission.where(:name => 'login').first]
-                
-role.save
-puts "Role: #{ role.name } created!"
-
-puts 'Setting up default usersdb/seeds.rb.'
-user = User.new :first_name            => 'Test',
-                :last_name             => 'User',
-                :email                 => 'user@example.com',
-                :password              => 'password',
-                :password_confirmation => 'password',
-                :role                  => adminRole
-
-user.save
-user.confirm!
-
-puts "User: #{ user.full_name } - #{ user.email } created and confirmed!"
+puts 'New user created: ' << user.email
