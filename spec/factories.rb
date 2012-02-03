1625fad8317e112882a29c26c020106e528e3554
Jose Diaz <jose.a.diaz@aguaseca.com>
2012-02-02 21:20:40 -0700

    spec'd out and defined a member

--- spec/factories.rb
+++ spec/factories.rb
@@ -6,23 +6,35 @@ FactoryGirl.define do
     password_confirmation   'password'
   end
   
-  factory :role do
-  end
-  
-  factory :skill do
-  end
-  
   factory :tool do
       name                  'Bench Grinder'
       identification_number '35498 322010'
       description           'Central Machinery 37822 (6" grinder)'
       location              'Woodworking Bench'
   end
-  
-  factory :tool_category do
-  end
-  
-  factory :users_skill do
+
+  factory :associate_member, :class => Member do
+    first_name        "Student"
+    last_name         "Member"
+    email             "student.member@heatsynclabs.org"
+    membership_level  "Associate"
+    active            true
   end
 
+  factory :basic_member, :class => Member do
+    first_name        "Basic"
+    last_name         "Member"
+    email             "basic.member@heatsynclabs.org"
+    membership_level  "Basic"
+    active            true
+  end
+
+  factory :premium_member, :class => Member do
+    first_name        "Premium"
+    last_name         "Member"
+    email             "premium.member@heatsynclabs.org"
+    membership_level  "Premium"
+    active            true
+  end
+  
 end
