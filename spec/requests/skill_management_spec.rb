c0e76cf14535115bfa0279e8e12ff381ed4b78a4
Ryan Rix <ry@n.rix.si>
2012-02-20 08:24:04 -0700

    T: Begin speccing out skill management

---
+++ spec/requests/skill_management_spec.rb
@@ -0,0 +1,33 @@
+require 'spec_helper'
+
+describe 'Interests' do
+
+  describe 'Adding interests' do
+
+    it 'should have a link to interests on the home page' do
+      
+    end
+
+    it 'should have a link to add new interests from the My Interests page' do
+      
+    end
+
+  end
+
+  describe 'Updating interests' do
+  end
+
+  describe 'Removing interests' do
+  end
+
+end
+
+describe 'Badges' do
+
+  describe 'Awarding badges' do
+  end
+
+  describe 'Checking badge permissions' do
+  end
+
+end
